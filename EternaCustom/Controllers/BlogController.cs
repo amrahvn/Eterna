f8d3c60696d66138c8e1366bb167f0638f7d7bbc
Amrahvn <amrahvn@code.edu.az>
2023-08-31 05:01:53 +0400

    added

---
+++ EternaCustom/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EternaCustom.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Single(int? id) 
+        {
+            return View();
+        }
+    }
+}
---
+++ EternaCustom/Controllers/PortfolioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EternaCustom.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
